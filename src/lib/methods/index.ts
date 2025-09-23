98bad5bb4a9bc89227118f0731717938c7cd8803
Nitesh <nitesh2@relinns.com>
2025-09-24 01:29:20 +0530

    [ADDED] Added twilio free account api

--- src/lib/methods/index.ts
+++ src/lib/methods/index.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { TWILIO_API, TWILIO_PASSWORD, TWILIO_FROM, TWILIO_USERNAME } from '../env-variables';
+import {
+	TWILIO_API,
+	TWILIO_PASSWORD,
+	TWILIO_FROM,
+	TWILIO_USERNAME,
+} from '../env-variables';
 
 export const generateOtp = (digits = 4) =>
 	Math.floor(10 ** (digits - 1) + Math.random() * 9 * 10 ** (digits - 1));
@@ -9,7 +14,9 @@ export const sendSms = async (
 	countryCode: string,
 	phone: string
 ) => {
-	const auth = Buffer.from(`${TWILIO_USERNAME}:${TWILIO_PASSWORD}`).toString('base64');
+	const auth = Buffer.from(`${TWILIO_USERNAME}:${TWILIO_PASSWORD}`).toString(
+		'base64'
+	);
 
 	const response = await axios.post(
 		`${TWILIO_API}`,
@@ -21,8 +28,8 @@ export const sendSms = async (
 		{
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
-				'Authorization': `Basic ${auth}`,
-			}
+				Authorization: `Basic ${auth}`,
+			},
 		}
 	);
 
